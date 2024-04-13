bda0161b8c5611da195fac8a14aa7b5ae17649c8
joaosouzaaa <joaoasouza982@gmail.com>
2024-04-13 17:42:00 -0300

    Feat: Adding Book mapping and Initial migration.

---
+++ src/DataLayer/CQRSPattern.DatabaseSettings/EntitiesMapping/BookMapping.cs
@@ -0,0 +1,32 @@
+﻿using CQRSPattern.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CQRSPattern.DatabaseSettings.EntitiesMapping;
+
+internal sealed class BookMapping : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.Title)
+               .IsRequired(true)
+               .HasColumnName("title")
+               .HasColumnType("varchar(150)");
+
+        builder.Property(b => b.Author)
+               .IsRequired(true)
+               .HasColumnName("author")
+               .HasColumnType("varchar(100)");
+
+        builder.Property(b => b.Gender)
+               .IsRequired(true)
+               .HasColumnName("gender");
+
+        builder.Property(b => b.PublicationDate)
+               .IsRequired(true)
+               .HasColumnName("publication_date")
+               .HasColumnType("datetime2");
+    }
+}
---
+++ src/DataLayer/CQRSPattern.DatabaseSettings/Migrations/20240413204053_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CQRSPattern.DatabaseSettings.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "varchar(150)", nullable: false),
+                    author = table.Column<string>(type: "varchar(100)", nullable: false),
+                    gender = table.Column<int>(type: "int", nullable: false),
+                    publication_date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}

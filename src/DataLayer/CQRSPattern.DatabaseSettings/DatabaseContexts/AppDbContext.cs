0efb67d3bbe9c53f73021ae90df1145663f5c068
joaosouzaaa <joaoasouza982@gmail.com>
2024-04-13 17:34:05 -0300

    Feat: Adding DbContext.

---
+++ src/DataLayer/CQRSPattern.DatabaseSettings/DatabaseContexts/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CQRSPattern.DatabaseSettings.DatabaseContexts;
+public sealed class AppDbContext(DbContextOptions<DbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+}

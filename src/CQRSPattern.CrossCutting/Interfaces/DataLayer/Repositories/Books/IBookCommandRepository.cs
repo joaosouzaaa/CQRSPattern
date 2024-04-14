03c9f89b948fec7ca09f0365a5ae725b603ac0d0
joaosouzaaa <joaoasouza982@gmail.com>
2024-04-13 21:28:29 -0300

    Feat: Adding UpdateBookCommandHandler.

--- src/CQRSPattern.CrossCutting/Interfaces/DataLayer/Repositories/Books/IBookCommandRepository.cs
+++ src/CQRSPattern.CrossCutting/Interfaces/DataLayer/Repositories/Books/IBookCommandRepository.cs
@@ -5,6 +5,7 @@ namespace CQRSPattern.CrossCutting.Interfaces.DataLayer.Repositories.Books;
 public interface IBookCommandRepository
 {
     void Add(Book book);
+    Task<Book?> GetByIdAsync(int id);
     void Update(Book book);
     Task<bool> ExistsAsync(int id);
     Task DeleteAsync(int id);

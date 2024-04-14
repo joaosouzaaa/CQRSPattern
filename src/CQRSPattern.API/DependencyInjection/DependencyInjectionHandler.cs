61c7d0bf171530c33ab2aa15a4dfad4fea8af3b3
joaosouzaaa <joaoasouza982@gmail.com>
2024-04-14 18:38:56 -0300

    Feat: Adding BookController.

--- src/CQRSPattern.API/DependencyInjection/DependencyInjectionHandler.cs
+++ src/CQRSPattern.API/DependencyInjection/DependencyInjectionHandler.cs
@@ -4,9 +4,10 @@ internal static class DependencyInjectionHandler
 {
     internal static void AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddCorsDependencyInjection();
+
         services.AddMediatR(options => options.RegisterServicesFromAssembly(Application.AssemblyReference.Assembly));
 
-        services.AddCorsDependencyInjection();
         services.AddSettingsDependencyInjection();
         services.AddDataDependencyInjection();
         services.AddOptionsDependencyInjection(configuration);
